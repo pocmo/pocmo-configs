b7af6a2948049efa5b93c49ac5fe85f2e7568fe8
Sebastian Kaspari <s.kaspari@googlemail.com>
2010-10-29 11:51:29 +0200

    git prompt update

--- bash/git-prompt.bash
+++ bash/git-prompt.bash
@@ -1,12 +1,11 @@
-
 function parse_git_dirty {
-    git diff --quiet HEAD &>/dev/null
-      [[ $? == 1 ]] && echo "*" || echo ""
+  git diff --quiet HEAD &>/dev/null
+  [[ $? == 1 ]] && echo "*" || echo ""
 }
 
 function parse_git_branch {
-    ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-      echo " ("${ref#refs/heads/}"$(parse_git_dirty))"
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo " ("${ref#refs/heads/}"$(parse_git_dirty))"
 }
 
 function parse_git_stash {
@@ -16,8 +15,8 @@ function parse_git_stash {
   fi
 }
 
-RED="\[\033[0;31m\]"
 YELLOW="\[\033[0;33m\]"
 NORMAL="\[\e[0m\]"
+RED="\[\033[1;31m\]"
 
-PS1="\u@\h:\w$YELLOW\$(parse_git_branch)$RED\$(parse_git_stash}$NORMAL\$ "
+PS1="\u@\h:\w$YELLOW\$(parse_git_branch)$RED\$(parse_git_stash)$NORMAL\$ "
