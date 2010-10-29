f70da97cdf290b61829ce56c5951a5f0d5fa2a75
Sebastian Kaspari <s.kaspari@googlemail.com>
2010-10-29 11:36:14 +0200

    added stash to git prompt

--- bash/git-prompt.bash
+++ bash/git-prompt.bash
@@ -1,3 +1,4 @@
+
 function parse_git_dirty {
     git diff --quiet HEAD &>/dev/null
       [[ $? == 1 ]] && echo "*" || echo ""
@@ -8,8 +9,15 @@ function parse_git_branch {
       echo " ("${ref#refs/heads/}"$(parse_git_dirty))"
 }
 
+function parse_git_stash {
+  stash_list=`git stash list 2> /dev/null`
+  if [ -n "$stash_list" ]; then
+    echo "!"
+  fi
+}
+
 RED="\[\033[0;31m\]"
 YELLOW="\[\033[0;33m\]"
 NORMAL="\[\e[0m\]"
 
-PS1="\u@\h:\w$YELLOW\$(parse_git_branch)$NORMAL\$ "
+PS1="\u@\h:\w$YELLOW\$(parse_git_branch)$RED\$(parse_git_stash}$NORMAL\$ "
